16bc3db193b3232512ddb21a8ca06850d0eb5983
Заботлин Дмитрий <d.zabotlin@ntc-epsilon.com>
2014-03-28 19:18:26 +0400

    Test fixes.

--- test_fsm.cpp
+++ test_fsm.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "fsm.hpp"
 
-struct ev_1_2 {};
-struct ev_2_3 {};
-struct ev_3_4 {};
-struct ev_4_5 {};
-struct ev_5_1 {};
-struct ev1 {};
+struct ev_1 {};
+struct ev_2 {};
+struct ev_3 {};
+struct ev_4 {};
+struct ev_5 {};
+struct ev_6 {};
 
 enum class st
 {
@@ -19,37 +19,37 @@ enum class st
 
 class test_fsm : public fsm::state_machine<test_fsm, st, 5, st::s_1>
 {
-    void on_ev_1(const ev_1_2& e) { (void)(e); printf("on_ev_1_2\n\r"); }
-    void on_ev_2(const ev_2_3& e) { (void)(e); printf("on_ev_2_3\n\r"); }
-    void on_ev_3(const ev_3_4& e) { (void)(e); printf("on_ev_3_4\n\r"); }
-    void on_ev_4(const ev_4_5& e) { (void)(e); printf("on_ev_4_5\n\r"); }
-    void on_ev_5(const ev_5_1& e) { (void)(e); printf("on_ev_5_1\n\r"); }
-    void on_ev1(const ev1& e) { (void)(e); printf("on_ev1\n\r"); }
+    void on_ev_1(const ev_1& e) { (void)(e); printf("event 1 -> "); }
+    void on_ev_2(const ev_2& e) { (void)(e); printf("event 2 -> "); }
+    void on_ev_3(const ev_3& e) { (void)(e); printf("event 3 -> "); }
+    void on_ev_4(const ev_4& e) { (void)(e); printf("event 4 -> "); }
+    void on_ev_5(const ev_5& e) { (void)(e); printf("event 5 -> "); }
+    void on_ev_6(const ev_6& e) { (void)(e); printf("event 6 -> "); }
 
-    bool g_ev_1(const ev_1_2& e) { (void)(e); printf("g_ev_1_2\n\r"); return true; }
-    bool g_ev_2(const ev_2_3& e) { (void)(e); printf("g_ev_2_3\n\r"); return true; }
-    bool g_ev_3(const ev_3_4& e) { (void)(e); printf("g_ev_3_4\n\r"); return true; }
-    bool g_ev_4(const ev_4_5& e) { (void)(e); printf("g_ev_4_5\n\r"); return true; }
-    bool g_ev_5(const ev_5_1& e) { (void)(e); printf("g_ev_5_1\n\r"); return true; }
-    bool g_ev1(const ev1& e) { (void)(e); printf("g_ev1\n\r"); return true; }
+    bool g_ev_1(const ev_1& e) { (void)(e); printf("event 1 guard -> "); return true; }
+    bool g_ev_2(const ev_2& e) { (void)(e); printf("event 2 guard -> "); return true; }
+    bool g_ev_3(const ev_3& e) { (void)(e); printf("event 3 guard -> "); return true; }
+    bool g_ev_4(const ev_4& e) { (void)(e); printf("event 4 guard -> "); return true; }
+    bool g_ev_5(const ev_5& e) { (void)(e); printf("event 5 guard -> "); return true; }
+    bool g_ev_6(const ev_6& e) { (void)(e); printf("event 6 guard -> "); return true; }
 
     template<typename ev>
-    bool g_false(const ev& e) { (void)(e); printf("g_false\n\r"); return false; }
+    bool g_false(const ev& e) { (void)(e); printf("false guard -> "); return false; }
 
     typedef test_fsm f;
 
     typedef transition_table<
 //         | Start  | Event |  Next  |  Callback  |     Guard   |
 // --------|--------|-------|--------|------------|-------------|
-        row< st::s_1, ev_1_2, st::s_2, &f::on_ev_1, &f::g_ev_1 >,
-        row< st::s_2, ev_2_3, st::s_3, &f::on_ev_2, &f::g_ev_2 >,
-        row< st::s_3, ev_3_4, st::s_4, &f::on_ev_3, &f::g_ev_3 >,
-        row< st::s_4, ev_4_5, st::s_5, &f::on_ev_4, &f::g_ev_4 >,
-        row< st::s_5, ev_5_1, st::s_1, &f::on_ev_5, &f::g_ev_5 >,
+        row< st::s_1, ev_1, st::s_2, &f::on_ev_1, &f::g_ev_1 >,
+        row< st::s_2, ev_2, st::s_3, &f::on_ev_2, &f::g_ev_2 >,
+        row< st::s_3, ev_3, st::s_4, &f::on_ev_3, &f::g_ev_3 >,
+        row< st::s_4, ev_4, st::s_5, &f::on_ev_4, &f::g_ev_4 >,
+        row< st::s_5, ev_5, st::s_1, &f::on_ev_5, &f::g_ev_5 >,
 // --------+--------+-------+--------+------------+-------------|
-        row< st::s_2, ev1,    st::s_4, &f::on_ev1,  &f::g_ev1 >,
-        row< st::s_3, ev1,    st::s_5, &f::on_ev1,  nullptr   >,
-        row< st::s_5, ev1,    st::s_1, &f::on_ev1,  &f::g_false >
+        row< st::s_2, ev_6, st::s_4, &f::on_ev_6, &f::g_ev_6 >,
+        row< st::s_3, ev_6, st::s_5, &f::on_ev_6, nullptr   >,
+        row< st::s_5, ev_6, st::s_1, &f::on_ev_6, &f::g_false >
     > transition_table_t;
 
 public:
@@ -94,13 +94,12 @@ int main(int argc, char** argv)
 
     printf("sizeof(test_fsm): %u\n\r", sizeof(test_fsm));
 
-    struct ev_1_2 ev12;
-    struct ev_2_3 ev23;
-    struct ev_3_4 ev34;
-    struct ev_4_5 ev45;
-    struct ev_5_1 ev51;
-
-    struct ev1 e1;
+    struct ev_1 ev1;
+    struct ev_2 ev2;
+    struct ev_3 ev3;
+    struct ev_4 ev4;
+    struct ev_5 ev5;
+    struct ev_6 ev6;
 
     st s = st::s_1;
 
@@ -109,41 +108,41 @@ int main(int argc, char** argv)
     out_state(fsm.state());
 
     // 1 -> 2 -> 3 -> 4 -> 5 -> 1
-    s = fsm.process_event(ev12);
+    s = fsm.process_event(ev1);
     out_state(s);
-    s = fsm.process_event(ev23);
+    s = fsm.process_event(ev2);
     out_state(s);
-    s = fsm.process_event(ev34);
+    s = fsm.process_event(ev3);
     out_state(s);
-    s = fsm.process_event(ev45);
+    s = fsm.process_event(ev4);
     out_state(s);
-    s = fsm.process_event(ev51);
+    s = fsm.process_event(ev5);
     out_state(s);
 
-    printf("TEST 2\n\r");
+    printf("\n\rTEST 2\n\r");
     printf("initial state: ");
     out_state(fsm.state());
 
     // 1 -> 2 -> 4 -> 5 -> 5
-    s = fsm.process_event(ev12);
+    s = fsm.process_event(ev1);
     out_state(s);
-    s = fsm.process_event(e1);
+    s = fsm.process_event(ev6);
     out_state(s);
-    s = fsm.process_event(ev45);
+    s = fsm.process_event(ev4);
     out_state(s);
-    s = fsm.process_event(e1);
+    s = fsm.process_event(ev6);
     out_state(s);
 
     fsm.reset();
-    printf("TEST 3\n\r");
+    printf("\n\rTEST 3\n\r");
     printf("initial state: ");
     out_state(fsm.state());
 
     // 1 -> 2 -> 3
-    s = fsm.process_event(ev12);
+    s = fsm.process_event(ev1);
     out_state(s);
-    s = fsm.process_event(ev23);
+    s = fsm.process_event(ev2);
     out_state(s);
-    s = fsm.process_event(e1);
+    s = fsm.process_event(ev6);
     out_state(s);
 }
